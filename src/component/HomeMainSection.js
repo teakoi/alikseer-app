1e4f4bc3906b6ab573cc4497a7c00f15b4bfadc5
Zaira Ramji <zairaram@gmail.com>
2024-03-24 18:37:22 -0600

    implemented reviews, FIX STARS

--- src/component/HomeMainSection.js
+++ src/component/HomeMainSection.js
@@ -3,10 +3,33 @@ import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
 import Productpage from "./Productpage";
 import Header from "./Header";
 import Homepage from "./Homepage";
-import reviews from 
+import reviews from  "../data/reviews"; //Import the reviews array
 
 
-function HomeMainSection(){
+
+const HomeMainSection = () =>{
+    const [DisplayedReviews, setDisplayedReviews] = useState([]);
+
+    useEffect(() =>{
+        const getDisplayedReviews = () => {
+            const shuffledReviews = [...reviews].sort(()=> 0.5 - Math.random()); //shuffle reviews
+            const selectedReviews = shuffledReviews.slice(0,2); // select 2 reviews
+            setDisplayedReviews(selectedReviews);
+        };
+        getDisplayedReviews(); //Call the function to select random reviews
+    }, []);
+
+    // Function to render star icons based on the number of stars
+    const renderStars = (stars) => {
+        const starIcons = [];
+        for (let i = 0; i < stars; i++) {
+            starIcons.push(<i key={i} className="fa-solid fa-star"></i>);
+        }
+        return starIcons;
+    };
+
+
+
     return(
         <main>
             
@@ -29,11 +52,16 @@ function HomeMainSection(){
  
             <section class="customer-reviews">
                 <h2>Customer Reviews</h2>
+                {DisplayedReviews.map((review, index) => (
+                    <div key={index} class="review">
+                        <p>{review.customerName}</p>
+                        <p>{review.reviewContent}</p>
+                        <p>Rating: {renderStars(review.stars)}</p>
+                    </div>
+                ))}
             </section>
 
         </main>
-
-
     );
 }
 
