1d2da767cda5c1bdb181f48ae40b50f1bb7d5a6a
Zaira Ramji <zairaram@gmail.com>
2024-04-08 15:22:47 -0700

    got rid of imports we dont need

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import Homepage from './component/Homepage.js'
-import logo from './logo.svg';
+
 import './App.css';
 import Productpage from './component/Productpage.js';
-import product from './data/products.js';
+
 import LoginPage from './component/LoginPage.js';
 
 function App() {
