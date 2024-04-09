5b9e1f9c97cb2645f2705daf54cd3dae0194fbf2
Zaira Ramji <zairaram@gmail.com>
2024-04-08 17:35:00 -0700

    chnaged

--- src/component/ProductList.js
+++ src/component/ProductList.js
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ProductItem from './ProductItem';
-import productsData from '../data/products';
 
 const ProductList = ({ addProductToCart }) => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    // Fetch products from the backend API
+    fetch('/api/products')
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error('Failed to fetch products');
+      })
+      .then(data => {
+        setProducts(data);
+      })
+      .catch(error => {
+        console.error('Error fetching products:', error);
+      });
+  }, []);
+
+
+
+
   return (
     <div className="product-list">
 
-      {productsData.map((product) => (
+      {products.map((product) => (
         <ProductItem 
           key={product.id} 
           product={product} 
